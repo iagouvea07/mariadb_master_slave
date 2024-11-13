03f386db66e4bc0f0560bdbdfdbc372de65c0eea
Igor Alves <116216770+iagouvea07@users.noreply.github.com>
2024-11-12 22:21:50 -0300

    Update commands.sql

--- commands.sql
+++ commands.sql
@@ -13,12 +13,12 @@ FLUSH PRIVILEGES;
 SHOW SLAVE STATUS;
 
 CHANGE MASTER TO
-    MASTER_HOST='10.182.0.7',
-    MASTER_PORT=3306,
-    MASTER_USER='replicator',
-    MASTER_PASSWORD='12345',
-    MASTER_LOG_FILE='binlog.000001', --binlog obtido no comando SHOW MASTER STATUS
-    MASTER_LOG_POS=333;  --position obtida no comando SHOW MASTER STATUS
+    MASTER_HOST='10.182.0.7',            --ip do banco master
+    MASTER_PORT=3306,                    --porta utilizada pelo banco master
+    MASTER_USER='replicator',            --usuario para replicar o banco de dados
+    MASTER_PASSWORD='12345',             --senha do usuario replicator
+    MASTER_LOG_FILE='binlog.000001',     --binlog obtido no comando SHOW MASTER STATUS
+    MASTER_LOG_POS=333;                  --position obtida no comando SHOW MASTER STATUS
 
 START SLAVE;
 
